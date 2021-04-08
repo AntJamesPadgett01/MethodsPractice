8f39bf2553419e71f7959feaeaa4f0f4f62010f1
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-08 14:34:21 -0500

    Task Completed

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MethodsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SayHello("Ralph", 32);
+            SayHello("Rick", 28);
+            SayHello("Rob", 24);
+
+        }
+
+        static void SayHello(string name, int age) //parameters/arguments
+        {
+            Console.WriteLine($"Hello {name} you are {age}.");// methods and functions interchangable.
+        }
+
+
+
+
+
+
+
+    }
+}
